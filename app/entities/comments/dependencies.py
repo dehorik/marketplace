144da8e257181c1b7b7cc914bee1436429fc982b
dehorik <tsiptsin.egor@gmail.com>
2024-08-27 14:56:37 +0700

    bag fix

--- app/entities/comments/dependencies.py
+++ app/entities/comments/dependencies.py
@@ -1,7 +1,8 @@
 from typing import Annotated
 from fastapi import Form, UploadFile, HTTPException, status
 
-from core.database import Session, CommentDataBase
+from core.config_reader import config
+from core.database import CommentDataBase
 from utils import FileWriter, FileReWriter, FileDeleter, Converter
 from entities.comments.models import CommentModel
 
@@ -23,24 +24,24 @@ class CreateComment:
                 )
 
         converter = Converter(CommentModel)
-        session = Session()
-        comment_db = CommentDataBase(session)
 
         if comment_photo:
-            comment_photo_path = str(FileWriter(FileWriter.comment_path, comment_photo.file.read()))
+            comment_photo_path = FileWriter(
+                config.getenv("COMMENT_PHOTO_PATH"),
+                comment_photo.file.read()
+            ).path
         else:
             comment_photo_path = None
 
-        comment = comment_db.create(
-            user_id,
-            product_id,
-            comment_rating,
-            comment_text,
-            comment_photo_path
-        )
-
-        comment_db.close()
-        self.comment = converter.serialization(comment)[0]
+        with CommentDataBase() as comment_db:
+            comment = comment_db.create(
+                user_id,
+                product_id,
+                comment_rating,
+                comment_text,
+                comment_photo_path
+            )
+            self.comment = converter.serialization(comment)[0]
 
 
 class UpdateComment:
@@ -57,54 +58,52 @@ class UpdateComment:
                     status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
                     detail='invalid file type'
                 )
-
-        session = Session()
-        comment_db = CommentDataBase(session)
         converter = Converter(CommentModel)
 
-        params = {
-            "comment_text": comment_text,
-            "comment_rating": comment_rating
-        }
-        params = {key: value for key, value in params.items() if value is not None}
-        comment = comment_db.update(comment_id, **params)
+        with CommentDataBase() as comment_db:
+            params = {
+                "comment_text": comment_text,
+                "comment_rating": comment_rating
+            }
+            params = {key: value for key, value in params.items() if value is not None}
+            comment = comment_db.update(comment_id, **params)
 
-        if not comment:
-            comment_db.close()
+            if not comment:
+                comment_db.close()
 
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail='incorrect comment_id'
-            )
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail='incorrect comment_id'
+                )
 
-        if comment_photo:
-            if comment[0][-1]:
-                FileReWriter(comment[0][-1], comment_photo.file.read())
-            else:
-                comment_photo_path = str(FileWriter(FileWriter.comment_path, comment_photo.file.read()))
-                comment = comment_db.update(comment_id, comment_photo_path=comment_photo_path)
+            if comment_photo:
+                if comment[0][-1]:
+                    FileReWriter(comment[0][-1], comment_photo.file.read())
+                else:
+                    comment_photo_path = FileWriter(
+                        config.getenv("COMMENT_PHOTO_PATH"),
+                        comment_photo.file.read()
+                    ).path
+                    comment = comment_db.update(comment_id, comment_photo_path=comment_photo_path)
 
-        comment_db.close()
-        self.comment = converter.serialization(comment)[0]
+            self.comment = converter.serialization(comment)[0]
 
 
 class DeleteComment:
     def __init__(self, comment_id: int):
         converter = Converter(CommentModel)
-        session = Session()
-        comment_db = CommentDataBase(session)
 
-        comment = comment_db.delete(comment_id)
-        if not comment:
-            comment_db.close()
+        with CommentDataBase() as comment_db:
+            comment = comment_db.delete(comment_id)
 
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail='incorrect comment_id'
-            )
+            if not comment:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail='incorrect comment_id'
+                )
 
-        if comment[0][-1]:
-            FileDeleter(comment[0][-1])
+            deleted_comment_photo_path = comment[0][-1]
+            if deleted_comment_photo_path:
+                FileDeleter(deleted_comment_photo_path)
 
-        comment_db.close()
-        self.comment = converter.serialization(comment)[0]
+            self.comment = converter.serialization(comment)[0]
