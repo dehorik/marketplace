75ad4edaf55d9877f9b1a4826d1ef44deadc4b24
dehorik <tsiptsin.egor@gmail.com>
2024-08-30 12:07:50 +0700

    work on auth

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,1 +1,4 @@
 from auth.redis_client import RedisClient, ConnectionData
+from auth.tokens import EncodeJWT, DecodeJWT
+from auth.hashing_psw import get_password_hash, verify_password
+from auth.models import SuccessfulAuthModel, UserCredentialsModel, TokensModel
---
+++ app/auth/models.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Field
+
+from entities.users.models import UserModel
+
+
+class TokensModel(BaseModel):
+    access_token: str
+    refresh_token: str
+
+
+class UserCredentialsModel(BaseModel):
+    user_name: str = Field(min_length=6, max_length=14)
+    user_password: str = Field(min_length=8, max_length=18)
+
+
+class SuccessfulAuthModel(BaseModel):
+    user: UserModel
+    tokens: TokensModel
